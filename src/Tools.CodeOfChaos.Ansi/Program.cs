63385f0eb7d6365a456dc12b1d18466a618e20c1
Anna Sas <anna.sas.dev@gmail.com>
2025-11-11 14:02:03 +0100

    Change: Package updates and build enhancements

--- src/Tools.CodeOfChaos.Ansi/Program.cs
+++ src/Tools.CodeOfChaos.Ansi/Program.cs
@@ -12,23 +12,23 @@ public static class Program {
     public static async Task Main(string[] args) {
         // Register & Build the parser
         //      Don't forget to add the current assembly if you built more tools for the current project
-        CliArgsParser parser = CliArgsBuilder.CreateFromConfig(
-            config => {
-                config.AddCommandsFromAssemblyEntrypoint<IAssemblyEntry>();
-            }
-        ).Build();
+        ICliParser parser = CliParser.CreateBuilder()
+            .AddFromAssembly<IAssemblyEntry>()
+            .Build();
 
         // We are doing this here because else the launchSettings.json file becomes a humongous issue to deal with.
-        //      Sometimes CLI params is not the answer.
-        //      Code is the true saviour
-        string projects = string.Join(";",
+        //      Sometimes CLI params are not the answer.
+        //      Code is the true savior
+        // const string projects = "";
+        string projects = string.Join(
+            ";",
             "CodeOfChaos.Ansi",
             "CodeOfChaos.Ansi.Generators"
         );
 
-        string oneLineArgs = InputHelper.ToOneLine(args).Replace("%PROJECTS%", projects);
+        string oneLineArgs = ArgsInputHelper.ToOneLine(args).Replace("%PROJECTS%", projects);
 
-        // Finally start executing
-        await parser.ParseAsync(oneLineArgs);
+        // Finally, start executing
+        await parser.ExecuteAsync(oneLineArgs);
     }
 }
